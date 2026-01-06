6440b46374caf41f9b1d856cbb85c336c6799dcc
Solomon Abu <75063694+solo-exe@users.noreply.github.com>
2026-01-06 20:12:42 +0100

    updates

--- src/main/java/com/sollo_scripts/banking_app/controller/AccountController.java
+++ src/main/java/com/sollo_scripts/banking_app/controller/AccountController.java
@@ -2,9 +2,11 @@ package com.sollo_scripts.banking_app.controller;
 
 import com.sollo_scripts.banking_app.dto.AccountDto;
 import com.sollo_scripts.banking_app.service.AccountService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +20,9 @@ public class AccountController {
         this.accountService = accountService;
     }
 
-    // Add Account RespAPI
+    // Add account rest api
     @PostMapping("/add")
-    public ResponseEntity<AccountDto> addAccount (AccountDto accountDto) {
+    public ResponseEntity<AccountDto> addAccount (@Valid @RequestBody AccountDto accountDto) {
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/sollo_scripts/banking_app/dto/AccountDto.java
+++ src/main/java/com/sollo_scripts/banking_app/dto/AccountDto.java
@@ -1,5 +1,7 @@
 package com.sollo_scripts.banking_app.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,6 +9,10 @@ import lombok.Data;
 @AllArgsConstructor
 public class AccountDto {
     private Long id;
+
+    @NotBlank(message = "Account Name is required")
     private String accountName;
+
+    @PositiveOrZero(message = "Balance cannot be negative")
     private double balance;
 }
